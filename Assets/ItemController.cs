faf2154af2ea93c70703120f143fea9f733e0263
kirimin <minkarakirimade@gmail.com>
2019-05-26 23:23:17 +0900

    First commit

---
+++ Assets/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-speed, 0, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D c) {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(x * speed, y * speed, 0);
+    }
+}
